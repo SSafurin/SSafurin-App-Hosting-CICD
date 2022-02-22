dadcae1b7195489d16ba98fad33361b56a0b2e2c
ssafurin <siman.safurin@atos.com>
2022-02-22 09:19:50 +0100

    dd

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<body>
+<?php
+$hostname = "ssamariadbserver.mariadb.database.azure.com";
+$username = "phpappuser@ssamariadbserver";
+$password = "MySQLAzure2017";
+$db = "sampledb";
+
+$dbconnect=mysqli_connect($hostname,$username,$password,$db);
+
+if ($dbconnect->connect_error) {
+  die("Database connection failed: " . $dbconnect->connect_error);
+}
+
+?>
+
+<table border="1" align="center">
+<tr>
+  <td>Reviewer Name</td>
+  <td>Stars</td>
+  <td>Details</td>
+</tr>
+
+<?php
+
+$query = mysqli_query($dbconnect, "SELECT * FROM user_review")
+   or die (mysqli_error($dbconnect));
+
+while ($row = mysqli_fetch_array($query)) {
+  echo
+   "<tr>
+    <td>{$row['reviewer_name']}</td>
+    <td>{$row['star_rating']}</td>
+    <td>{$row['details']}</td>
+   </tr>\n";
+
+}
+
+?>
+</table>
+</body>
+</html>
+
+
+
+
+
